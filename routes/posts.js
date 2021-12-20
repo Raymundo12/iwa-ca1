3dcd02f355d4e3eb12505ab950455dc7a7a68825
Raymundo12 <raymundo.olverap@gmail.com>
2021-12-20 22:39:14 +0000

    bosyparser

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,16 @@
 const express = require ('express');
 
 const router = express.Router();
+const Post = require('../models/Post')
+
 
 router.get('/posts', (req,res)=>{
     res.send('Welcome posts');
+
+});
+
+router.post('/', (req, res) =>{
+    console.log(req,body);
 
 });
 
