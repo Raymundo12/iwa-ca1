b85ee0078e2835093e6518836d49b0e646413a32
Raymundo12 <raymundo.olverap@gmail.com>
2021-12-21 22:39:32 +0000

    new model

--- models/Post.js
+++ models/Post.js
@@ -2,10 +2,16 @@ const mongoose = require ('mongoose');
 const { intersects } = require('semver');
 
 const PostSchema = mongoose.Schema({
-   title: String,
-   description: String,
-   date: Date,
+   title: {
+      type: String,
+      required : true
+   },
 
+   description: {
+      type: String,
+      required: true
+   },
+         
 });
 
 module.exports = mongoose.model('Posts', PostSchema);
