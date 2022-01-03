f3372c1089b3cad12679d488773c5cdad5f7bf92
Raymundo12 <raymundo.olverap@gmail.com>
2022-01-03 22:07:09 +0000

    models

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require ('express')
+const mongoose = require ('mongoose')
 const booksRouter = require('./routes/books')
 const app = express()
+
+mongoose.connect('mongodb://localhost/blog')
 
 app.set('view engine', 'ejs')
 
