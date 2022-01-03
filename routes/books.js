1025dfeeb17681e6a9750ec3e4d4ba03e7bb4760
Raymundo12 <raymundo.olverap@gmail.com>
2022-01-03 20:19:59 +0000

    books

--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,8 @@
 const express = require ('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('In Books')
+router.get('/new', (req, res) => {
+    res.render('books/new')
 })
 
 module.exports = router 
