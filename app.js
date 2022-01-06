07233e9e400838eeb6d0cbefa9b39eb8847f6117
Raymundo12 <raymundo.olverap@gmail.com>
2022-01-06 23:27:26 +0000

    routes

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const morgan = require('morgan')
 // settings 
 app.set('port', 8000);
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 
 //middlewares
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended: false}))
+
 
 
 //Routes
