82e6fc6b03aae4a35db72945677aec8d060cfd17
Raymundo12 <raymundo.olverap@gmail.com>
2022-01-09 14:15:27 +0000

    app.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const app = require('../app');
+const app = require('./app');
 
 async function main () {
   await app.listen(app.get('port'));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,12 @@
-const { Router} = require('express')
+const { Router} = require('express');
+const res = require('express/lib/response');
+const { route } = require('../app');
 const router = Router()
+
+route.get('/', (req, res) => {
+    
+})
+   res.send('hello world')
 
 module.exports = router;
 
