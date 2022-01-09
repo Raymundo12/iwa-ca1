43d0cc0b2f9fba19db6598e272bc48a195376d6b
Raymundo12 <raymundo.olverap@gmail.com>
2022-01-09 14:47:02 +0000

    structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,5 @@ const res = require('express/lib/response');
 const { route } = require('../app');
 const router = Router()
 
-route.get('/', (req, res) => {
-    
-})
-   res.send('hello world')
-
 module.exports = router;
 
